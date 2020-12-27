93efe8bce748a3fc238fd099ce3fd7bbc73d72d6
HanJin Park <phj9020@hotmail.com>
2020-12-27 20:11:47 +0900

    server setup"

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.listen('9040', function(){
-    console.log("listing")
-})
+const PORT = 4000;
+
+function handleListening(){
+  console.log(`listening... on : http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
