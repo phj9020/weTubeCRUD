8d8588fe48f3a7ee8405f55e36478d7b9005c7e8
HanJin Park <phj9020@hotmail.com>
2021-01-06 19:09:07 +0900

    document url fixed, add middleware for uploads

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const app = express();
 // middleware
 app.use(helmet());
 app.set('view engine', 'pug');
+app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
