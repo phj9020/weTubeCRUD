112ac72a2d0227cdf16d5771540eaea3c315e6b9
HanJin Park <phj9020@hotmail.com>
2021-01-06 17:24:28 +0900

    videoUploaded added using multer

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,16 @@
 
 import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req, res) => {
-    res.render("home",{pageTitle: "Home", videos});
+export const home = async(req, res) => {
+    try {
+        // Bring all Videos from DB
+        const videos = await Video.find({});
+        res.render("home",{pageTitle: "Home", videos});
+    } catch(error) {
+        console.log(error);
+        res.render("home",{pageTitle: "Home", videos});
+    }
 }
 
 export const search = (req, res) => {
@@ -16,13 +24,20 @@ export const getUpload = (req, res) => {
     res.render("upload", {pageTitle: "Upload"});
 }
 
-export const postUpload = (req, res) => {
+export const postUpload = async(req, res) => {
     const { 
-        body : {file, title, description} 
+        body : { title, description },
+        file : { path }
     } = req;
-
-    // To do : Upload and save video
-    res.redirect(routes.videoDetail(324294))
+    
+    const newVideo = await Video.create({
+        fileUrl : path,
+        title : title,
+        description : description
+    })
+    console.log(newVideo);
+  
+    res.redirect(routes.videoDetail(newVideo.id))
 }
 
 
