818c257511e9ba4fd07118e73037180063036846
HanJin Park <phj9020@hotmail.com>
2021-01-18 13:16:45 +0900

    expand btn function added

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,7 +2,7 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.querySelector("#jsPlayButton");
 const volumnBtn = document.querySelector("#jsVolumeButton");
-
+const expandBtn = document.querySelector("#jsExpandButton");
 
 function handlePlayClick(){
     if(videoPlayer.paused) {
@@ -25,9 +25,25 @@ function handleVolumeClick(){
     }
 }
 
+function exitFullScreen(){
+    expandBtn.innerHTML = `<i class="fas fa-expand"></i>`;
+    expandBtn.addEventListener("click", goFullScreen);
+    document.exitFullscreen()
+        .catch(error => Promise.resolve(error));
+    expandBtn.removeEventListener("click",exitFullScreen);
+}
+
+function goFullScreen(){
+    videoContainer.requestFullscreen();
+    expandBtn.innerHTML = `<i class="fas fa-compress"></i>`;
+    expandBtn.removeEventListener("click", goFullScreen);
+    expandBtn.addEventListener("click",exitFullScreen);
+}
+
 function init(){
-    playBtn.addEventListener("click", handlePlayClick)
-    volumnBtn.addEventListener("click", handleVolumeClick)
+    playBtn.addEventListener("click", handlePlayClick);
+    volumnBtn.addEventListener("click", handleVolumeClick);
+    expandBtn.addEventListener("click", goFullScreen);
 }
 
 // videoContainer가 존재하면 init함수 호출
